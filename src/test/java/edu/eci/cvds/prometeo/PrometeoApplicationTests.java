e3090435b37b69948e46cd1c1eddf8e51678e0b4
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-05-12 06:47:56 -0500

    chore: add mock interaction into application test and modify application.properties to accept SSL

--- src/test/java/edu/eci/cvds/prometeo/PrometeoApplicationTests.java
+++ src/test/java/edu/eci/cvds/prometeo/PrometeoApplicationTests.java
@@ -2,8 +2,12 @@ package edu.eci.cvds.prometeo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import jakarta.activation.DataSource;
 
 @SpringBootTest
+@MockBean(DataSource.class)
 class PrometeoApplicationTests {
 
 	@Test
