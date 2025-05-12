ca692c7b1bf9c4619a1150c1311312b47deb01e6
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-05-12 10:46:00 -0500

    chore: update test instance to fix ci-cd

--- src/test/java/edu/eci/cvds/prometeo/PrometeoApplicationTests.java
+++ src/test/java/edu/eci/cvds/prometeo/PrometeoApplicationTests.java
@@ -1,17 +1,14 @@
 package edu.eci.cvds.prometeo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
-import jakarta.activation.DataSource;
-
-@WebMvcTest
+@SpringBootTest
+@ActiveProfiles("test")
 class PrometeoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
