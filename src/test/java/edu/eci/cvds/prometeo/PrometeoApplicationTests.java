d48066f3bcf7d08dbbff88d2b4cdd1ddd6a2fbe5
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-05-12 06:54:01 -0500

    feat: add webMvc test in context loads

--- src/test/java/edu/eci/cvds/prometeo/PrometeoApplicationTests.java
+++ src/test/java/edu/eci/cvds/prometeo/PrometeoApplicationTests.java
@@ -1,13 +1,13 @@
 package edu.eci.cvds.prometeo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import jakarta.activation.DataSource;
 
-@SpringBootTest
-@MockBean(DataSource.class)
+@WebMvcTest
 class PrometeoApplicationTests {
 
 	@Test
